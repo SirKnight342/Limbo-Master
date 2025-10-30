83ed1d84df6594511475be22ec9748c67130d399
SirKnight342 <115183911+SirKnight342@users.noreply.github.com>
2025-10-31 00:55:25 +0800

    Add files via upload

---
+++ limbos32-master/spawn-all.py
@@ -0,0 +1,21 @@
+from os import system
+from _thread import start_new_thread
+from time import sleep
+
+
+counted = 0
+
+
+def threadeded():
+    global counted
+    counted += 1
+    system("python main.py")
+    counted -= 1
+
+
+for _ in range(8):
+    start_new_thread(threadeded, ())
+    sleep(0.23)
+
+while counted:
+    sleep(0.1)
